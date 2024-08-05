b1225a1674f24123c7e18d4b9099f2c6e3135048
sothea007 <nemsothea13@gmail.com>
2024-08-05 08:55:10 +0700

    Initial Commit

---
+++ Customizing the Appearance of Notifications in Swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Customizing the Appearance of Notifications in Swift
+//
+//  Created by sothea007 on 5/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Customizing the Appearance of Notifications in Swift/Customizing_the_Appearance_of_Notifications_in_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Customizing_the_Appearance_of_Notifications_in_SwiftApp.swift
+//  Customizing the Appearance of Notifications in Swift
+//
+//  Created by sothea007 on 5/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Customizing_the_Appearance_of_Notifications_in_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
