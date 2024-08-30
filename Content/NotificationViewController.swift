aab2852dab51562ca99b7e64fba1fdd2de5cf02d
sothea007 <nemsothea13@gmail.com>
2024-08-30 17:25:25 +0700

    - Finish push notification with Video

--- Content/NotificationViewController.swift
+++ Content/NotificationViewController.swift
@@ -9,53 +9,87 @@ import UIKit
 import UserNotifications
 import UserNotificationsUI
 import AVKit
-import XCDYouTubeKit
+//import WebKit
+//import SafariServices
 
 class NotificationViewController: UIViewController, UNNotificationContentExtension {
-  
-    
-
     // MARK: - @IBOutlet
-    @IBOutlet var label             : UILabel?
-    @IBOutlet weak var playerView   : UIView!
-    @IBOutlet weak var imageView    : UIImageView!
+    @IBOutlet weak var playerView       : UIView!
+    @IBOutlet weak var imageView        : UIImageView!
+    @IBOutlet weak var buttonAction     : UIButton!
     
-    // MARK: - @Properties
-    var playerController    : AVPlayerViewController!
-    let standardHeight      : CGFloat = 432
-   
-    
-    
+    @IBOutlet weak var titleLabel       : UILabel!
+    @IBOutlet weak var descriptionLabel : UILabel!
+    @IBOutlet weak var viewDetailLabel  : UILabel!
+    // MARK: - Properties
+//    var webView             : WKWebView?
+    var player              : AVPlayer?
+    var playerLayer         : AVPlayerLayer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any required interface initialization here.
- 
+     
+        // intitButton
+        self.intitButton()
+       
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        self.stopVideo()
+        self.playerLayer?.removeFromSuperlayer()
+        self.player = nil
+        self.player?.removeObserver(self, forKeyPath: "timeControlStatus")
     }
     
+    deinit {
+        self.player?.removeObserver(self, forKeyPath: "timeControlStatus")
+    }
+    /// Apple doc: https://developer.apple.com/documentation/objectivec/nsobject/1416553-observevalue
+    ///observeValue(forKeyPath:of:change:context:)
+    ///Informs the observing object when the value at the specified key path relative to the observed object has changed.
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "timeControlStatus"{
+            self.updatePlayPauseButton()
+        }
+    }
+    
+    @objc private func playPauseTapped() {
+        if self.player?.timeControlStatus == .playing {
+            self.player?.pause()
+        } else {
+            self.player?.play()
+        }
+        self.updatePlayPauseButton()
+    }
+    func intitButton() {
+        self.buttonAction.setImage(UIImage(systemName: "play.fill"), for: .normal)
+        self.buttonAction.addTarget(self, action: #selector(self.playPauseTapped), for: .touchUpInside)
+        self.buttonAction.backgroundColor = UIColor.white.withAlphaComponent(0.7)
+        self.buttonAction.layer.cornerRadius = self.buttonAction.frame.height / 2
+        self.buttonAction.clipsToBounds = true
+    }
     func didReceive(_ notification: UNNotification) {
-
-      
         
         let content = notification.request.content
-        print("content",content)
-        
         
         // Case 1 with image
-//        if let urlImageString = content.userInfo["attachment-url"] as? String {
-//            self.imageView.isHidden = false
-//            self.playerView.isHidden = true
-//            self.imageView(for: urlImageString)
-//        }
-//        
-        // Case 2 with video
-        playerController = AVPlayerViewController()
-        if let videoId = content.userInfo["videoId"] as? String {
-     
-
-            self.videoView(for: videoId)
+        if let urlString = content.userInfo["attachment-url"] as? String {
+            
+            if urlString.contains(".mp4") {
+                // Handle video case
+                self.imageView.isHidden = true
+                self.playerView.isHidden = false
+                self.playVideo(for: urlString)
+                //set description
+                self.getValueVideoDescription(with: content)
+            } else {
+                // Handle image case
+                self.imageView.isHidden = false
+                self.playerView.isHidden = true
+                self.imageView(for: urlString)
+            }
         }
-       
     }
     //MARK: Case 1 with image
     private func imageView(for urlImageString : String) {
@@ -73,47 +107,98 @@ class NotificationViewController: UIViewController, UNNotificationContentExtensi
             }
         }
     }
-    //MARK: Case 2 with video
-    private func videoView(for videoId : String ) {
-        
     
-    XCDYouTubeClient.default().getVideoWithIdentifier("RDWxtwBYIy0ZU&start_radio=1") { (video, error) in
-           
-            
-            if let error = error {
-                print(error.localizedDescription)
-                return
-            }
-            
-            guard let video = video else {
-                print("No video found")
-                return
-            }
-            
-            let streamURLS = video.streamURLs
-            if let url = streamURLS[XCDYouTubeVideoQuality.medium360] ?? streamURLS[XCDYouTubeVideoQuality.small240] ?? streamURLS[XCDYouTubeVideoQuality.HD720] ?? streamURLS[18] {
-                self.setupPlayer(with: url)
-            } else {
-                print("No valid stream URL found")
-            }
+    // MARK: - Handle YouTube Video
+    private func playVideo(for urlString: String) {
+        guard let url = URL(string: urlString) else {
+#if DEBUG
+            print("Invalid URL string: \(urlString)")
+#endif
+            return
+        }
+        
+        // Option 1: Using WKWebView
+        //           self.openVideoWithWebkit(with: url)
+        
+        // Option 2: Using SFSafariViewController
+        //           self.openWithSFSafariView(with: url)
+        
+        // Option 3: Utilize AVPlayer for Video Playback
+        self.openWithPlay(with: url)
+        
+    }
+    func getValueVideoDescription(with content: UNNotificationContent) {
+    
+        let aps     = content.userInfo["aps"] as? NSDictionary
+        let alert   = aps?["alert"] as? NSDictionary
+        
+        if let title = alert?["title"] as? String,
+           let body = alert?["body"] as? String, let description = content.userInfo["description"] as? String  {
+            self.titleLabel.text        = title
+            self.descriptionLabel.text  = body
+            self.viewDetailLabel.text   = description
         }
     }
-    private func setupPlayer(with url: URL) {
-        let player = AVPlayer(url: url)
-        playerController.player = player
-        playerController.view.frame = self.playerView.bounds
-        playerView.addSubview(playerController.view)
-        addChild(playerController)
-        playerController.didMove(toParent: self)
-        player.play()
+    func updatePlayPauseButton() {
+        if self.player?.timeControlStatus == .playing {
+            self.buttonAction.setImage(UIImage(systemName: "pause.fill"), for: .normal)
+            
+        } else {
+            self.buttonAction.setImage(UIImage(systemName: "play.fill"), for: .normal)
+        }
     }
-
+   // MARK: - Helper Functions
+//    func openVideoWithWebkit(with url: URL) {
+//        if self.webView == nil {
+//            self.webView = WKWebView(frame: self.playerView.bounds)
+//            if let webView = self.webView {
+//                webView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+//                self.playerView.addSubview(webView)
+//            }
+//        }
+//        self.webView?.load(URLRequest(url: url))
+//    }
+    
+    /**
+     @class SFSafariViewController
+     A view controller for displaying web content in a Safari-like interface with some of Safari’s features.
+     */
+//    func openWithSFSafariView(with url: URL) {
+//        // If you prefer using SFSafariViewController instead of WKWebView, use the following:
+//        let safariVC = SFSafariViewController(url: url)
+//        present(safariVC, animated: true, completion: nil)
+//    }
+    
+    // Initialize AVPlayer with the video URL
+    func openWithPlay(with url: URL) {
+        self.player = AVPlayer(url: url)
+        
+        // Create an AVPlayerLayer and add it to the playerView
+        self.playerLayer = AVPlayerLayer(player: player)
+        self.playerLayer?.frame = playerView.bounds
+        self.playerLayer?.videoGravity = .resizeAspectFill
+        self.playerView.layer.addSublayer(playerLayer!)
+   
+        // Start video playback
+//           self.player?.play()
+        self.playerView.bringSubviewToFront(buttonAction)
+        
+        // Start observing the player's status
+        self.player?.addObserver(self, forKeyPath:"timeControlStatus", options: [.initial, .new], context: nil)
+    }
+    
+   
+    func stopVideo() {
+        self.player?.pause()
+        self.player?.seek(to: .zero)
+        self.updatePlayPauseButton()
+    }
 }
 
 extension URLSession {
     
     class func downloadImage(atURL url: URL, withCompletionHandler completionHandler: @escaping (Data?, NSError?) -> Void) {
-        let dataTask = URLSession.shared.dataTask(with: url) { (data, urlResponse, error) in
+        let dataTask = URLSession.shared.dataTask(with: url) { (data, _, error) in
             completionHandler(data, nil)
         }
         dataTask.resume()
--- Service/NotificationService.swift
+++ Service/NotificationService.swift
@@ -10,48 +10,26 @@ import UserNotifications
 class NotificationService: UNNotificationServiceExtension {
 
     var contentHandler: ((UNNotificationContent) -> Void)?
+    
     var bestAttemptContent: UNMutableNotificationContent?
 
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        
         self.contentHandler = contentHandler
         bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
         
-        if let bestAttemptContent = bestAttemptContent {
-            // Modify the notification content here...
-            bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
-            var urlString:String? = nil
-            if let urlImageString = request.content.userInfo["attachment-url"] as? String {
-                urlString = urlImageString
+        // 1. Check for custom content
+        guard let bestAttemptContent = bestAttemptContent, let mediaURL = bestAttemptContent.userInfo["attachment-url"] as? String, let attachmentURL = URL(string: mediaURL) else {
+            return
+        }
+        // 2. Download the image and pass it to attachments if not nil
+        self.downloadImageFrom(url: attachmentURL) { (attachment) in
+            if attachment != nil {
+                bestAttemptContent.attachments = [attachment!]
+                contentHandler(bestAttemptContent)
             }
-            
-            if let videoId = request.content.userInfo["videoId"] as? String {
-                // Process video URL or video data
-                bestAttemptContent.userInfo["videoId"] = videoId
-            }
-            
-            if urlString != nil, let fileUrl = URL(string: urlString!) {
-                print("fileUrl: \(fileUrl)")
-                
-                guard let imageData = NSData(contentsOf: fileUrl) else {
-                    contentHandler(bestAttemptContent)
-                    return
-                }
-                guard let attachment = UNNotificationAttachment.saveImageToDisk(fileIdentifier: "image.jpg", data: imageData, options: nil) else {
-                    print("error in UNNotificationAttachment.saveImageToDisk()")
-                    contentHandler(bestAttemptContent)
-                    return
-                }
-              
-                
-                bestAttemptContent.attachments = [ attachment ]
-            }
-            
-            
-            
-            contentHandler(bestAttemptContent)
         }
     }
-    
     override func serviceExtensionTimeWillExpire() {
         // Called just before the extension will be terminated by the system.
         // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
@@ -61,23 +39,51 @@ class NotificationService: UNNotificationServiceExtension {
     }
 
 }
-extension UNNotificationAttachment {
+
+// MARK: - Multi Media Helper
+extension NotificationService {
     
-    static func saveImageToDisk(fileIdentifier: String, data: NSData, options: [NSObject : AnyObject]?) -> UNNotificationAttachment? {
-        let fileManager = FileManager.default
-        let folderName = ProcessInfo.processInfo.globallyUniqueString
-        let folderURL = NSURL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(folderName, isDirectory: true)
-        
-        do {
-            try fileManager.createDirectory(at: folderURL!, withIntermediateDirectories: true, attributes: nil)
-            let fileURL = folderURL?.appendingPathComponent(fileIdentifier)
-            try data.write(to: fileURL!, options: [])
-            let attachment = try UNNotificationAttachment(identifier: fileIdentifier, url: fileURL!, options: options)
-            return attachment
-        } catch let error {
-            print("error \(error)")
+    private func downloadImageFrom(url: URL, with completionHandler: @escaping (UNNotificationAttachment?) -> Void) {
+        let task = URLSession.shared.downloadTask(with: url) { (downloadedUrl, response, error) in
+            guard let downloadedUrl = downloadedUrl else {
+                completionHandler(nil)
+                return
+            }
+            let fileExtension = self.determineFileExtension(from: url, response: response)
+            
+            // 1. Application directory path
+            var urlPath = URL(fileURLWithPath: NSTemporaryDirectory())
+            // 2. Validate the content extension. If file is unsupported. Fallback will happend and basic Push Notifcation will be delivered
+            let urlPathExt = ProcessInfo.processInfo.globallyUniqueString + fileExtension // .png, . gif, .jpg, .mp4.
+            urlPath = urlPath.appendingPathComponent(urlPathExt)
+            
+            do {
+                // 3. Move downloadedUrl to newly created urlPath
+                try FileManager.default.moveItem(at: downloadedUrl, to: urlPath)
+                let attachment = try UNNotificationAttachment(identifier: "attachment", url: urlPath, options: nil)
+                completionHandler(attachment)
+            }
+            catch {
+                completionHandler(nil)
+            }
+        }
+        task.resume()
+    }
+    private func determineFileExtension(from url: URL, response: URLResponse?) -> String {
+        if let mimeType = response?.mimeType {
+            if mimeType == "video/mp4" {
+                return ".mp4"
+            } else if mimeType == "image/jpeg" {
+                return ".jpg"
+            } else if mimeType == "image/png" {
+                return ".png"
+            } else if mimeType == "image/gif" {
+                return ".gif"
+            }
         }
         
-        return nil
+        // If MIME type is not available, fallback to file extension from the URL
+        return url.pathExtension.isEmpty ? ".dat" : ".\(url.pathExtension)"
     }
 }
+
